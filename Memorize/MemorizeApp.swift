ce0ba28edb8950d54f428e2fcd41144253a88d38
Murilo47 <murilo.faverol@gmail.com>
2024-02-23 10:37:45 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Murilo Favero Oliveira on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
