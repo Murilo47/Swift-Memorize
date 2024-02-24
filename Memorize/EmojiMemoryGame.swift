a1b83221571204e42ea659862ca44b8bc9c3d6ee
Murilo47 <murilo.faverol@gmail.com>
2024-02-24 11:20:35 -0300

    XCode Project Update

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,36 @@
 //  Created by Murilo Favero Oliveira on 23/02/24.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "👹", "😱", "☠️", "🍭"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> { //cannot infer return type
+        MemoryGame(numberOfPairsOfCards: 6) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+    //create a memory game and specifies that the generic type is a String
+    //private protects the model from changes coming from View
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) { //intent functionn //_ means we have no external calling
+        model.choose(card)
+    }
+}
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -8,15 +8,32 @@
 import Foundation
 
 struct MemoryGame<CardContent> {
-    var cards: Array<Card>
+    private(set) var cards: Array<Card>
+    //private(set) means that it is private to set, but it is public to see/look at it
     
-    func choose(card: Card) {
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        //add numberOfPairsOfCards x 2 cards
+        
+        for pairIndex in 0..<max(2, numberOfPairsOfCards) { //we don't use the index
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
         
     }
     
+    func choose(_ card: Card) {
+        
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
     struct Card{
-        var isFaceUp: Bool
-        var isMatched: Bool
-        var content: CardContent //don't care type
+        var isFaceUp = true
+        var isMatched = false
+        let content: CardContent //don't care type
     }
 }
